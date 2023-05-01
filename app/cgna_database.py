5171076c5968785890bd1b14117200db55beeaf4
Claudio Correa <clcorrea@udec.cl>
2023-04-30 23:12:08 -0400

    4-30-23

--- app/cgna_database.py
+++ app/cgna_database.py
@@ -1,13 +1,43 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    current_app,
+    g,
+    flash,
 )
 
-bp = Blueprint('cgna_database', __name__, url_prefix='/')
+bp = Blueprint("cgna_database", __name__, url_prefix="/")
 
-@bp.route('/', methods=['GET'])
+from werkzeug.exceptions import abort
+from app.db import get_db
+
+
+@bp.route("/", methods=["GET"])
 def index():
-    return render_template('cgna_database/index.html')
+    return render_template("cgna_database/index.html")
 
-@bp.route('/', methods=['POST'])
+
+@bp.route("/", methods=["GET", "POST"])
 def search_genes():
-    return ''
+    if request.method == "POST":
+        name_genes = request.form.get("name_genes")
+        specie = request.form.get("specie")
+        chromosome = request.form.get("chromosomes")
+        error = None
+        if not specie and not chromosome and not name_genes:
+            error = "invalid query..."
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                "insert into queries (id_specie, id_chromosome, name_gen, created_by)"
+                " values (%s,%s,%s,%s)",
+                (specie, chromosome, name_genes, 1),
+            )
+            db.commit()
+            return render_template("cgna_database/show_genes.html")
+    return redirect(url_for("cgna_database.index"))
--- run.py
+++ run.py
@@ -1,51 +1,43 @@
 import subprocess
 import os
 
+
 def run_flask():
     if os.path.exists("venv/Scripts/activate"):
         print("Inicializando...")
-        print('start APP in Flask..')
-        os.environ['FLASK_APP'] = 'app'
-        os.environ['FLASK_DATABASE_HOST'] = 'localhost'
-        os.environ['FLASK_DATABASE_PASSWORD'] = 'lukemaster'
-        os.environ['FLASK_DATABASE_USER'] = 'root'
-        os.environ['FLASK_DATABASE'] = 'cgna_database'
+        print("start APP in Flask..")
+        os.environ["FLASK_APP"] = "app"
+        subprocess.call(["flask", "init-db"])
 
     else:
         print("virtualizado aplicacion...")
-        subprocess.call(["python","-m","venv","venv"])
-        subprocess.call([".","venv/Scripts/activate"])
-        subprocess.call(["pip","install","Flask"])
-        subprocess.call(["pip","install","werkzeug"])
-        subprocess.call(["pip","install","mysql-connector-python"])
+        subprocess.call(["python", "-m", "venv", "venv"])
+        subprocess.call([".", "venv/Scripts/activate"])
+        subprocess.call(["pip", "install", "Flask"])
+        subprocess.call(["pip", "install", "werkzeug"])
+        subprocess.call(["pip", "install", "mysql-connector-python"])
+        subprocess.call(["pip", "install", "python-dotenv"])
 
-        print('start APP in Flask..')
-        os.environ['FLASK_APP'] = 'app'
-        os.environ['FLASK_DATABASE_HOST'] = 'localhost'
-        os.environ['FLASK_DATABASE_PASSWORD'] = 'lukemaster'
-        os.environ['FLASK_DATABASE_USER'] = 'root'
-        os.environ['FLASK_DATABASE'] = 'cgna_database'
-        print('start database')
-        subprocess.call(["flask","init-db"])
-
-
+        print("start APP in Flask..")
+        os.environ["FLASK_APP"] = "app"
+        print("start database")
+        subprocess.call(["flask", "init-db"])
 
     # pip install werkzeug
     # pip install mysql-connector-python
     # python -m venv venv
     # . venv/Scripts/activate
     # pip install Flask
-    
-    
-    print('start database')
-    
-    
-    print('start APP as development..')
-    os.environ['FLASK_DEBUG'] = 'on'
-    print('flask run..')
-    subprocess.call(["flask","run"])
-    
-    #subprocess.call(["set"])
-    print('Done')
-    
+
+    print("start database")
+
+    print("start APP as development..")
+    os.environ["FLASK_DEBUG"] = "on"
+    print("flask run..")
+    subprocess.call(["flask", "run"])
+
+    # subprocess.call(["set"])
+    print("Done")
+
+
 run_flask()
