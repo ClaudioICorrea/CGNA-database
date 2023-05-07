ab87165f2704a98d6ebdb4ed21fb5a12d0c7b649
Claudio Correa <clcorrea@udec.cl>
2023-05-07 01:34:24 -0400

    5-7-23;1:34am

--- app/cgna_database.py
+++ app/cgna_database.py
@@ -16,22 +16,59 @@ def index():
 def _login():
     return render_template("cgna_database/auth/_login.html")
 
+def search_database(id_specie,querie,c,db):
+    where_is=None
+    id_querie=None
+    
+    # buscamos primeramente en la tabla de chromosomas 
+    c.execute(
+        'SELECT id_chromosome, alias FROM chromosomes WHERE id_specie = %s',
+    (id_specie,)
+    )
+    data = c.fetchall()
+    for dato in data:
+        ### buscamos si existen coincidencias con id_chromosomes
+        if dato['id_chromosome'] == querie:
+            where_is = 'chromosomes'
+            id_querie = dato['id_chromosome']
+            return where_is, id_querie
+        ### buscamos si existen coincidencias con los alias que tiene el chromosoma 
+        alias=dato['alias'].split(",")
+        for element in alias:
+            if querie == element:
+                where_is = 'chromosomes'
+                id_querie = dato['id_chromosome']
+                return where_is, id_querie
+        # buscamos primeramente en la tabla de chromosomas 
+    c.execute(
+    'SELECT id_genes, name_gen FROM genes'
+    )
+    data = c.fetchall()
+    for dato in data:
+        if dato['id_genes'] == querie:
+            where_is = 'genes'
+            id_querie = dato['id_genes']
+            return where_is, id_querie
+    return where_is, id_querie
+def show_genes():
+    return redirect(url_for("cgna_database.index"))
 
 @bp.route("/", methods=["GET", "POST"])
 def search_genes():
     if request.method == "POST":
         querie = request.form.get("querie")
-        specie = request.form.get("specie")
+        id_specie = request.form.get("specie")
         data=[]
         error = None
-        if not specie and not querie:
+        if not id_specie and not querie:
             error = "invalid query..."
         if error is not None:
             flash(error)
         else:
             db, c = get_db()
-            id_specie = int(specie) if specie else None
+            id_specie = int(id_specie) if id_specie else None
             querie = str(querie) if querie else None
+            ######################Guardamos la consulta en BD####################
             if g.user:
                 c.execute(
                     "INSERT INTO queries (id_specie, created_by,querie)"
@@ -45,26 +82,44 @@ def search_genes():
                     (id_specie,1,querie),
                 )
             db.commit()
+            #############Buscamos la informacion de planta##############################
+            c.execute(
+                    'SELECT * FROM species WHERE id_specie = %s',
+                (id_specie,) 
+                )
+            specie = c.fetchall()
+            db.commit()
+            #################busquemos queries en la base de dato####################### 
             if querie is None:
+                # no hay ninguna especificación de lo que se quiere
                 querie =""
                 c.execute(
-                    'SELECT id_chromosome, gbig, number_genes, size, alias FROM chromosomes WHERE id_specie = %s',
-                (specie,) 
+                   'SELECT id_chromosome, gbig, number_genes, size, alias FROM chromosomes WHERE id_specie = %s',
+                (id_specie,) 
                 )
                 data = c.fetchall()
-                c.execute(
-                    'SELECT * FROM species WHERE id_specie = %s',
-                (specie,) 
-                )
-                specie = c.fetchall()
-                print(data)
-                print(specie)
+            else:
+                id_querie, where_is =search_database(id_specie,querie,c,db)
+                print('estoy aqui !!',id_querie,where_is)
+                
+                    
+                    #if dato['id_chromosome'] == querie:
+                     #   where_is = 'chromosomes'
+                     #   id_querie = dato['id_chromosome']
+
             
-            return render_template("cgna_database/show_genes.html", querie = querie, specie=specie, data=data)
+            return render_template("cgna_database/show_info/show_specie.html", querie = querie, specie=specie, data=data)
     return redirect(url_for("cgna_database.index"))
 
-@bp.route("/show_genes", methods=["GET","POST"])
-def show_genes():
+
+
+    
+    
+    
+    
+    
+    
+    
     ## mostrar algo en pantalla 
     ## recuperar la ultima consulta
     ## busqueda de la consulta en la base de datos  en el orden specie-> chromosoma -> genes
